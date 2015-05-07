a20c0830f0ec904e6256f7f2343b58468f8b8dbd
PaulStoffregen <paul@pjrc.com>
2015-05-07 00:41:41 -0700

    Import Arduino 1.6.4

---
+++ app/src/processing/app/macosx/ThinkDifferent.java
@@ -0,0 +1,116 @@
+/* -*- mode: java; c-basic-offset: 2; indent-tabs-mode: nil -*- */
+
+/*
+  Part of the Processing project - http://processing.org
+
+  Copyright (c) 2007 Ben Fry and Casey Reas
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package processing.app.macosx;
+
+import com.apple.eawt.*;
+import processing.app.Base;
+import processing.app.Editor;
+
+import java.io.File;
+import java.util.List;
+
+
+/**
+ * Deal with issues related to thinking different. This handles the basic
+ * Mac OS X menu commands (and apple events) for open, about, prefs, etc.
+ * <p/>
+ * Based on OSXAdapter.java from Apple DTS.
+ * <p/>
+ * As of 0140, this code need not be built on platforms other than OS X,
+ * because of the new platform structure which isolates through reflection.
+ */
+public class ThinkDifferent {
+
+  private static final int MAX_WAIT_FOR_BASE = 10000;
+
+  static public void init() {
+    Application application = Application.getApplication();
+    application.setAboutHandler(new AboutHandler() {
+      @Override
+      public void handleAbout(AppEvent.AboutEvent aboutEvent) {
+        if (waitForBase()) {
+          Base.INSTANCE.handleAbout();
+        }
+      }
+    });
+    application.setPreferencesHandler(new PreferencesHandler() {
+      @Override
+      public void handlePreferences(AppEvent.PreferencesEvent preferencesEvent) {
+        if (waitForBase()) {
+          Base.INSTANCE.handlePrefs();
+        }
+      }
+    });
+    application.setOpenFileHandler(new OpenFilesHandler() {
+      @Override
+      public void openFiles(final AppEvent.OpenFilesEvent openFilesEvent) {
+        if (waitForBase()) {
+          for (File file : openFilesEvent.getFiles()) {
+            try {
+              Base.INSTANCE.handleOpen(file);
+              List<Editor> editors = Base.INSTANCE.getEditors();
+              if (editors.size() == 2 && editors.get(0).getSketch().isUntitled()) {
+                Base.INSTANCE.handleClose(editors.get(0));
+              }
+            } catch (Exception e) {
+              throw new RuntimeException(e);
+            }
+          }
+        }
+      }
+    });
+    application.setQuitHandler(new QuitHandler() {
+      @Override
+      public void handleQuitRequestWith(AppEvent.QuitEvent quitEvent, QuitResponse quitResponse) {
+        if (waitForBase()) {
+          if (Base.INSTANCE.handleClose(Base.INSTANCE.getActiveEditor())) {
+            quitResponse.performQuit();
+          } else {
+            quitResponse.cancelQuit();
+          }
+        }
+      }
+    });
+  }
+
+  private static boolean waitForBase() {
+    int slept = 0;
+    while (Base.INSTANCE == null) {
+      if (slept >= MAX_WAIT_FOR_BASE) {
+        return false;
+      }
+      sleep(100);
+      slept += 100;
+    }
+    return true;
+  }
+
+  private static void sleep(int millis) {
+    try {
+      Thread.sleep(100);
+    } catch (InterruptedException e) {
+      //ignore
+    }
+  }
+
+}
---
+++ app/test/processing/app/macosx/SystemProfilerParserTest.java
@@ -0,0 +1,78 @@
+/*
+ * This file is part of Arduino.
+ *
+ * Copyright 2015 Arduino LLC (http://www.arduino.cc/)
+ *
+ * Arduino is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As a special exception, you may use this file as part of a free software
+ * library without restriction.  Specifically, if other files instantiate
+ * templates or use macros or inline functions from this file, or you compile
+ * this file and link it with other files to produce an executable, this
+ * file does not by itself cause the resulting executable to be covered by
+ * the GNU General Public License.  This exception does not however
+ * invalidate any other reasons why the executable file might be covered by
+ * the GNU General Public License.
+ */
+
+package processing.app.macosx;
+
+import org.junit.Test;
+import processing.app.TestHelper;
+
+import static org.junit.Assert.assertEquals;
+
+public class SystemProfilerParserTest {
+
+  @Test
+  public void shouldCorrectlyParse() throws Exception {
+    String output = TestHelper.inputStreamToString(SystemProfilerParserTest.class.getResourceAsStream("system_profiler_output.txt"));
+
+    assertEquals("0X2341_0X0044", new SystemProfilerParser().extractVIDAndPID(output, "/dev/cu.usbmodemfa121"));
+    assertEquals("0X2341_0X0044", new SystemProfilerParser().extractVIDAndPID(output, "/dev/tty.usbmodemfa121"));
+
+    output = TestHelper.inputStreamToString(SystemProfilerParserTest.class.getResourceAsStream("system_profiler_output2.txt"));
+
+    assertEquals("0X2341_0X8036", new SystemProfilerParser().extractVIDAndPID(output, "/dev/cu.usbmodemfd131"));
+    assertEquals("0X2341_0X8036", new SystemProfilerParser().extractVIDAndPID(output, "/dev/tty.usbmodemfd131"));
+
+    output = TestHelper.inputStreamToString(SystemProfilerParserTest.class.getResourceAsStream("system_profiler_output3.txt"));
+
+    assertEquals("0X2341_0X8041", new SystemProfilerParser().extractVIDAndPID(output, "/dev/cu.usbmodemfd121"));
+    assertEquals("0X2341_0X8041", new SystemProfilerParser().extractVIDAndPID(output, "/dev/tty.usbmodemfd121"));
+
+    output = TestHelper.inputStreamToString(SystemProfilerParserTest.class.getResourceAsStream("system_profiler_output4.txt"));
+
+    assertEquals("0X2341_0X0041", new SystemProfilerParser().extractVIDAndPID(output, "/dev/cu.usbmodem411"));
+    assertEquals("0X2341_0X0041", new SystemProfilerParser().extractVIDAndPID(output, "/dev/tty.usbmodem411"));
+
+    output = TestHelper.inputStreamToString(SystemProfilerParserTest.class.getResourceAsStream("system_profiler_output5.txt"));
+
+    assertEquals("0X2341_0X8041", new SystemProfilerParser().extractVIDAndPID(output, "/dev/cu.usbmodem621"));
+    assertEquals("0X2341_0X8041", new SystemProfilerParser().extractVIDAndPID(output, "/dev/tty.usbmodem621"));
+
+    output = TestHelper.inputStreamToString(SystemProfilerParserTest.class.getResourceAsStream("system_profiler_output6.txt"));
+
+    assertEquals("0X2341_0X8041", new SystemProfilerParser().extractVIDAndPID(output, "/dev/cu.usbmodem1421"));
+    assertEquals("0X2341_0X8041", new SystemProfilerParser().extractVIDAndPID(output, "/dev/tty.usbmodem1421"));
+
+    output = TestHelper.inputStreamToString(SystemProfilerParserTest.class.getResourceAsStream("system_profiler_output7.txt"));
+
+    assertEquals("0X2341_0X8036", new SystemProfilerParser().extractVIDAndPID(output, "/dev/cu.usbmodem24131"));
+    assertEquals("0X2341_0X8036", new SystemProfilerParser().extractVIDAndPID(output, "/dev/tty.usbmodem24131"));
+    assertEquals("0X0403_0X6015", new SystemProfilerParser().extractVIDAndPID(output, "/dev/cu.usbserial-DN0031EV"));
+    assertEquals("0X0403_0X6015", new SystemProfilerParser().extractVIDAndPID(output, "/dev/tty.usbserial-DN0031EV"));
+  }
+}
